cb6e16069e13be81b6f522d098dd7b872e090b68
ABCHDK <118878901+ABCHDK@users.noreply.github.com>
2024-10-08 11:01:23 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Pr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRaschet = new System.Windows.Forms.Button();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonZak = new System.Windows.Forms.Button();
+            this.textBoxresult2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRaschet
+            // 
+            this.buttonRaschet.Location = new System.Drawing.Point(12, 53);
+            this.buttonRaschet.Name = "buttonRaschet";
+            this.buttonRaschet.Size = new System.Drawing.Size(86, 39);
+            this.buttonRaschet.TabIndex = 0;
+            this.buttonRaschet.Text = "Рассчитать";
+            this.buttonRaschet.UseVisualStyleBackColor = true;
+            this.buttonRaschet.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(12, 12);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(45, 20);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(123, 12);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(40, 20);
+            this.textBoxB.TabIndex = 3;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(223, 12);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(46, 20);
+            this.textBoxC.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(63, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X^2+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(182, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "X+";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxResult.Location = new System.Drawing.Point(12, 116);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(186, 20);
+            this.textBoxResult.TabIndex = 7;
+            this.textBoxResult.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // buttonZak
+            // 
+            this.buttonZak.Location = new System.Drawing.Point(149, 53);
+            this.buttonZak.Name = "buttonZak";
+            this.buttonZak.Size = new System.Drawing.Size(86, 39);
+            this.buttonZak.TabIndex = 8;
+            this.buttonZak.Text = "Закрыть";
+            this.buttonZak.UseVisualStyleBackColor = true;
+            this.buttonZak.Click += new System.EventHandler(this.buttonZak_Click);
+            // 
+            // textBoxresult2
+            // 
+            this.textBoxresult2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxresult2.Location = new System.Drawing.Point(12, 160);
+            this.textBoxresult2.Multiline = true;
+            this.textBoxresult2.Name = "textBoxresult2";
+            this.textBoxresult2.Size = new System.Drawing.Size(186, 20);
+            this.textBoxresult2.TabIndex = 7;
+            this.textBoxresult2.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(275, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "=0";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 192);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonZak);
+            this.Controls.Add(this.textBoxresult2);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.buttonRaschet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRaschet;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button buttonZak;
+        private System.Windows.Forms.TextBox textBoxresult2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBoxA.Text);
+            double b = double.Parse(textBoxB.Text);
+            double c = double.Parse(textBoxC.Text);
+
+            double dick = Math.Pow(b, 2) - 4 * a * c;
+            if (dick > 0)
+            {
+                // Два корня
+                double x1 = (-b + Math.Sqrt(dick)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(dick)) / (2 * a);
+                textBoxResult.Text = $"x1 = {x1:F2}";
+                textBoxresult2.Text = $"x2 = {x2:F2}";
+
+            }
+            else if( dick == 0)
+            {
+                // Два совпавших корня
+                double x3 = (-b + Math.Sqrt(dick)) / (2 * a);
+                textBoxResult.Text = $"x1 = {x3:F2}";
+                textBoxresult2.Clear();
+            }
+            else
+            {
+                // Нет действительных корней
+                textBoxResult.Text = "Уравнение не имеет корней.";
+                textBoxresult2.Clear();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonZak_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBoxResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
